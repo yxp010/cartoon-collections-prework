ce8b4685fc3e70db7a918fe9d18dfd39c6e60ca8
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 02:43:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  number = 1
+  dwarves.each do |dwarve|
+    puts "#{number}. #{dwarve}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(foods)# code an argument here
   # Your code here
+  foods.collect do |food|
+    food.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |call|
+    call.count > 4
+  end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find do |food|
+    cheese_types.include?(food)
+  end
 end
